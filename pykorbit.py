865201bf131153f9b04a86b1aa8f16dd780a2e74
jerome89 <jaerom89@gmail.com>
2017-08-22 19:03:14 +0900

    Updated Sources

--- pykorbit.py
+++ pykorbit.py
@@ -85,23 +85,73 @@ class KorbitApi():
         else:
             return False
     
+    ##################### 조회 API ########################
+    
     # Returns current User Information
     def getUserInfo(self):
-        return self.requestGet(path="user/info", headers=self.headers)
+        return self.requestGet(path = "user/info", headers = self.headers)
     
     # Returns current CurrencyPair (currencyPair : 'btc_krw', 'eth_krw', 'etc_krw', 'xrp_krw', 'bch_krw') 
     # 기본값 : currencyPair = 'btc_krw'
     def getCurrencyPair(self, currencyPair = 'btc_krw'):
-        return self.requestGet(path="ticker/detailed?currency_pair={}".format(currencyPair))
+        return self.requestGet(path = "ticker/detailed?currency_pair={}".format(currencyPair))
     
     # 매도 / 매수 호가 조회 (currencyPair : 'btc_krw', 'eth_krw', 'etc_krw', 'xrp_krw', 'bch_krw')
     # 기본값 : currencyPair = 'btc_krw'
     def getOrderbook(self, currencyPair = 'btc_krw'):
-        return self.requestGet(path="orderbook?currency_pair={}".format(currencyPair))
+        return self.requestGet(path = "orderbook?currency_pair={}".format(currencyPair))
    
     # 체결 내역 조회 
     # currencyPair : 'btc_krw', 'eth_krw', 'etc_krw', 'xrp_krw', 'bch_krw'
     # time : 'hour'(최근 1시간 체결 내역), 'minute'(최근 1분 체결 내역), 'day'(최근 하루 체결 내역)
     # 기본값 : currencyPair = 'btc_krw', time = 'hour'
     def getTransactions(self, currencyPair = 'btc_krw', time = 'hour'):
-        return self.requestGet(path="transactions?currency_pair={}&time={}".format(currencyPair, time))
+        return self.requestGet(path = "transactions?currency_pair={}&time={}".format(currencyPair, time))
+    
+    ######################################################
+    
+    
+    
+    ##################### 주문 API ########################
+    
+    # 매수 주문
+    # Returns orderId, status, currency_pair Fields
+    # currencyPair : 'btc_krw', 'eth_krw', 'etc_krw', 'xrp_krw', 'bch_krw'
+    # orderType : 'limit'(호가 주문), 'market'(시장가 주문)
+    # price : 호가 주문에 사용하는 parameter
+    # coinAmount : 매수하고자 하는 코인의 수량. 호가 주문인 경우에는 반드시 입력해야한다. 시장가 주문인 경우에는 자동으로 생성.
+    # fiatAmount : 시장가 주문인 경우에만 사용하는 구매에 쓰일 원화량
+    # nonce : api 호출마다 반드시 증가시켜야하는 value. 동일 주문을 방지하기 위함.
+    def orderCoin(self, currencyPair = None, orderType = None, 
+                  price = 0, coinAmount = 0, fiatAmount = 0, nonce = None):
+        if currencyPair is None or orderType is None or fiatAmount == 0 or nonce is None:
+            print("구매할 코인 / 주문 형태 / 코인 수량 / 원화 / nonce 등을 입력해주세요")
+            if orderType == 'limit' and price == 0:
+                print("호가 주문입니다. 코인 가격을 입력해주세요")
+            if orderType == 'market' and fiatAmount == 0:
+                print("시장가 주문입니다. 구매 가격을 입력해주세요.")
+        postData = {
+            'currency_pair': currencyPair,
+            'type': orderType,
+            'price': str(price),
+            'coin_amount': str(coinAmount),
+            'nonce': str(nonce),
+            'fiat_amount': str(fiatAmount)
+        }
+        return self.requestPost(path="user/orders/buy", data = postData, headers = self.headers)
+    
+    # 매도 주문
+    # Returns orderId, status, currency_pair Fields
+    # currencyPair : 'btc_krw', 'eth_krw', 'etc_krw', 'xrp_krw', 'bch_krw'
+    # orderType : 'limit'(호가 주문), 'market'(시장가 주문)
+    # price : 매도 주문에 사용할 가격. 호가 주문에만 사용.
+    # coinAmount : 매도하고자 하는 코인의 수량.
+    def sellCoin(self, currencyPair = None, orderType = None, price = 0, coinAmount = 0, nonce = None):
+        postData = {
+            'currency_pair': currencyPair,
+            'type': orderType,
+            'price': str(price),
+            'coin_amount': str(coinAmount),
+            'nonce': str(nonce)
+        }
+        return self.requestPost(path="user/orders/sell", data = postData, headers = self.headers)
